189cc29464f04d06ddaadd61966d38e52fe5933f
gt <kore207@naver.com>
2019-09-16 23:59:14 +0900

    D

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -10,10 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel? //'?' , '!' 모두 Optional을 뜻한다.
+    var userIsInTheMiddleOfTyping = false
+    //swift에서 모든 property는 초기값을 가져야 한다.
+    
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle! //읽기만 가능하도록 let 변수를 사용한다. , '!'를 붙여서 값을 꺼내오는것을 명시한다. (optional 이 아니다)
-        print("touched \(digit) digit")
-        //Optional 이라는 type 이 있다.  (not set(nil), set) swift는 타입을 추정한다.
+//        print("touched \(digit) digit")
+//        //Optional 이라는 type 이 있다.  (not set(nil), set) swift는 타입을 추정한다.
+        
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display!.text!
+            display!.text = textCurrentlyInDisplay + digit
+        } else{
+            display!.text = digit
+        }
+        userIsInTheMiddleOfTyping = true  
     }
 }
 
